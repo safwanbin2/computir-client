8822fd414434ee843fdde8a8f0a6ed9a643026bd
safwanbin2 <safwanridwan321@gmail.com>
2024-05-22 00:48:03 +0600

    base_url change

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
-  base_url: "http://localhost:5000/api/v1",
-  // base_url: "https://rantir.vercel.app/api/v1",
+  // base_url: "http://localhost:5000/api/v1",
+  base_url: "https://rantir.vercel.app/api/v1",
 };
